946fbda8e33cd031543611b05954fbe40cf64297
somidotun <106350605+somidotun@users.noreply.github.com>
2025-05-26 21:06:35 +0100

    mobile nav

--- src/app/(component)/nav/(mobile)/(menu)/menu.tsx
+++ src/app/(component)/nav/(mobile)/(menu)/menu.tsx
@@ -1,33 +1,88 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import icons8 from "@/app/images/icons8.png";
 import { LuChevronRight } from "react-icons/lu";
-import Buttons from "@/app/props/buttons";
+import Buttons from "@/app/props/buttonWhite";
+import ButtonBlack from "@/app/props/buttonBlack";
+import { motion, AnimatePresence } from "motion/react";
+import NewsMain from "./newsFeatured/newsMain";
 
 const Menu = () => {
+  // openNews
+  const [openNews, setOpenNews] = useState<boolean>(false);
+
+  const handleOpenNews = () => {
+    setOpenNews((prev) => !prev);
+  };
+
+  // animation variant
+  const variant = {
+    // initial
+    hidden: {
+      opacity: [0, 0.2, 0.4, 0.6, 0.8, 1],
+      x: "100vw",
+      transition: {
+        duration: 1,
+      },
+    },
+
+    // animate
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+
+    // exit
+    leave: {
+      opacity: [1, 0.8, 0.6, 0.4, 0.2, 0],
+      x: "100vw",
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
   return (
     <section
-      className="fixed top-0 bottom-0 bg-slate-600 
-     w-[80%] right-0 px-5 py-3"
+      className="fixed top-0 bottom-0  
+     w-[100%] right-0 flex flex-row"
     >
-      <div className="pt-10 flex flex-col gap-16">
+      <div className="bg-[#0000002a] w-[25%]"></div>
+      <div
+        className="pt-16 flex flex-col px-3 py-3
+      bg-white gap-16 w-[75%]"
+      >
         {/* nav links */}
-        <ul className="flex flex-col gap-5 font-normal">
-          <li className="flex flex-row gap-1">
+        <ul className="flex flex-col gap-5 font-normal text-xl">
+          {/* news and featured */}
+          <li
+            onClick={handleOpenNews}
+            className="flex flex-row gap-1
+           hover:text-gray-500"
+          >
             News & Featured
-            <LuChevronRight className=" h-6" />
+            <LuChevronRight className=" h-7" />
           </li>
-          <li className="flex flex-row gap-1">
+
+          {/* men */}
+          <li className="flex flex-row gap-1 hover:text-gray-500">
             Men
-            <LuChevronRight className=" h-6" />
+            <LuChevronRight className=" h-7" />
           </li>
-          <li className="flex flex-row gap-1">
+
+          {/* women */}
+          <li className="flex flex-row gap-1 hover:text-gray-500">
             Women
-            <LuChevronRight className=" h-6" />
+            <LuChevronRight className=" h-7" />
           </li>
-          <li className="flex flex-row gap-1">
+
+          {/* kids */}
+          <li className="flex flex-row gap-1 hover:text-gray-500">
             Kids
-            <LuChevronRight className=" h-6" />
+            <LuChevronRight className=" h-7" />
           </li>
         </ul>
 
@@ -45,18 +100,31 @@ const Menu = () => {
         </div>
 
         {/* text */}
-        <div className="w-[90%] flex flex-col gap-8">
+        <div className="w-[90%] flex flex-col gap-10">
           <p>
             Become a Nike Member for the best products, inspiration and stories
             in sport.<b>Learn more</b>
           </p>
 
           <div className="flex flex-row gap-7">
-            <Buttons style="bg-black text-white" text=" Join Us" />
-            <Buttons style="bg-white text-black" text="Sign in" />
+            <ButtonBlack text=" Join Us" />
+            <Buttons style="" text="Sign in" />
           </div>
         </div>
       </div>
+
+      <AnimatePresence>
+        {openNews && (
+          <motion.div
+            variants={variant}
+            initial="hidden"
+            animate="visible"
+            exit="leave"
+          >
+            <NewsMain />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
   );
 };
--- src/app/(component)/nav/(mobile)/mobileNav.tsx
+++ src/app/(component)/nav/(mobile)/mobileNav.tsx
@@ -1,60 +1,109 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import { CiHeart } from "react-icons/ci";
 import { FaSearch } from "react-icons/fa";
 import { FaCartPlus } from "react-icons/fa6";
 import one from "../../../images/one.jpg";
-import { IoIosMenu } from "react-icons/io";
 import { GrClose } from "react-icons/gr";
 import Menu from "./(menu)/menu";
+import { MdOutlinePersonOutline } from "react-icons/md";
+import { motion, AnimatePresence } from "framer-motion";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const MobileNav = () => {
-  // open nav
+  // open menu
   const [open, setOpen] = useState<boolean>(true);
 
   const handleOpen = () => {
     setOpen((prev) => !prev);
   };
 
+  // animation variant
+  const variant = {
+    // initial
+    hidden: {
+      opacity: [0, 0.2, 0.4, 0.6, 0.8, 1],
+      x: "100vw",
+      transition: {
+        duration: 1,
+      },
+    },
+
+    // animate
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+
+    // exit
+    leave: {
+      opacity: [1, 0.8, 0.6, 0.4, 0.2, 0],
+      x: "100vw",
+      transition: {
+        duration: 1,
+      },
+    },
+  };
   return (
-    <section className="flex flex-row justify-between px-3">
-      {/* jordon logo */}
-      <Link href="/" className="relative">
-        <p>
-          <Image src={one} alt="jordan logo" width={70} height={30} priority />
-        </p>
-      </Link>
+    <section className="p-3">
+      <nav className="flex flex-row justify-between relative">
+        {/* jordon logo */}
+        <Link href="/">
+          <p>
+            <Image
+              src={one}
+              alt="jordan logo"
+              width={70}
+              height={30}
+              priority
+            />
+          </p>
+        </Link>
 
-      {/* icons */}
+        {/* icons */}
 
-      <div className="flex flex-row gap-5">
-        <p>
-          <FaSearch className="h-10 text-2xl" />
-        </p>
+        <div className="flex flex-row gap-3">
+          <p>
+            <FaSearch className="h-10 text-2xl" />
+          </p>
 
-        <p>
-          <CiHeart className="h-10 text-2xl" />
-        </p>
+          <p>
+            <MdOutlinePersonOutline className="h-10 text-2xl" />
+          </p>
 
-        <p>
-          <FaCartPlus className="h-10 text-2xl" />
-        </p>
+          <p>
+            <FaCartPlus className="h-10 text-2xl" />
+          </p>
 
-        <div onClick={handleOpen} className="z-50">
-          {!open ? (
-            <p>
-              <GrClose className="h-10 text-2xl" />
-            </p>
-          ) : (
-            <p>
-              <IoIosMenu className="h-10 text-2xl" />
-            </p>
-          )}
+          <div onClick={handleOpen} className="z-50">
+            {open ? (
+              <p>
+                <GrClose className="h-10 text-2xl" />
+              </p>
+            ) : (
+              <p>
+                <RxHamburgerMenu className="h-10 text-2xl" />
+              </p>
+            )}
+          </div>
         </div>
+      </nav>
 
-        {!open && <Menu />}
-      </div>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            variants={variant}
+            initial="hidden"
+            animate="visible"
+            exit="leave"
+          >
+            <Menu />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
   );
 };
